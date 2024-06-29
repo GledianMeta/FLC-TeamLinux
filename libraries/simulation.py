b7799f8b4be84ba22f38bab31e9445c2f37f920f
salvatoregesualdo <salvigesualdo@libero.it>
2024-06-29 17:03:07 +0200

    start

--- libraries/simulation.py
+++ libraries/simulation.py
@@ -6,25 +6,38 @@ import threading
 class Simulation:
 
     def __init__(self, begin, end, step_duration, n_steps):
-        self.busy = False
         self.n_steps = n_steps
         self.semaphore = threading.Semaphore(1)  # Creiamo un semaforo con un contatore iniziale di 1
-        libsumo.start(["sumo", "-c", f".{CFG_PATH + SUMOCFG}", "-b", begin, "-e", end, "--step-length", step_duration])
-        self.doSteps(n_steps)
+        libsumo.start(["sumo", "-c", "test_simulation.sumocfg", "-b", str(begin), "-e", str(end), "--step-length",
+                       str(step_duration)])
+        self.do_steps(n_steps)
 
     def simulation_step(self, n_steps):
-        self.n_steps = n_steps
-        self.doSteps(n_steps)
-
-    def isBusy(self):
-        return self.busy
-
-    def doSteps(self, n_steps):
-        if n_steps == -1:
+        if self.is_busy():
             return
+        self.n_steps = n_steps
+        self.do_steps(n_steps)
+
+    def stop_simulation(self):
+        libsumo.close()
+
+    def is_busy(self):
+        return True if self.semaphore._value==0 else False
+
+    def do_steps(self, n_steps):
+
         self.semaphore.acquire()  # Acquisisce il semaforo prima di eseguire i passi della simulazione
+
+        if n_steps == -1:
+            # execute all steps of the simulation
+            return
         try:
-            for step in range(n_steps):
-                libsumo.simulationStep()
-        finally:
-            self.semaphore.release()  # Rilascia il semaforo dopo aver completato i passi
+            thread = threading.Thread(target=self.execute_steps, args=(self.n_steps,))
+            thread.start()
+        except RuntimeError:
+            return
+
+    def execute_steps(self, n_steps):
+        for step in range(n_steps):
+            libsumo.simulationStep()
+        self.semaphore.release()  # Rilascia il semaforo dopo aver completato i passi
