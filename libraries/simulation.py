b1e8d9fd684099bf515a6e465dea1d55885f2a23
Davide Savoia <44394074+davexhardware@users.noreply.github.com>
2024-06-30 12:46:21 +0200

    output corrections, simulation and start fixes for concurrancy

--- libraries/simulation.py
+++ libraries/simulation.py
@@ -5,19 +5,26 @@ from consts import *
 class Simulation:
 
     # Constructor
-    def __init__(self, begin, end, step_duration, n_steps):
+    def __init__(self):
+        self.semaphore = threading.Semaphore(
+            1)  # Semaphore Initialized with a count of 1 => This ensures that only one thread can acquire the semaphore at a time, preventing concurrent execution of simulation steps.
+
+    def configure(self, begin, end, step_duration, n_steps):
         self.n_steps = n_steps
-        self.semaphore = threading.Semaphore(1)  # Semaphore Initialized with a count of 1 => This ensures that only one thread can acquire the semaphore at a time, preventing concurrent execution of simulation steps.
-        libsumo.start(["sumo", "-c", CFG_PATH + SUMOCFG, "-b", str(begin), "-e", str(end), "--step-length", str(step_duration)])
+        libsumo.start(
+            ["sumo", "-c", CFG_PATH + SUMOCFG, "-b", str(begin), "-e", str(end), "--step-length", str(step_duration)])
         self.do_steps(n_steps)
-
-
-
+        self.kill_thread=False
 
     # Method which will be called to stop the simualtion
     def stop_simulation(self):
-        libsumo.close()
-        return "Simulation stopped"
+        try:
+            if self.semaphore.value==1:
+                libsumo.close()
+                self.kill_thread=True
+        except:
+            return False
+        return True
 
 
     def is_busy(self):
@@ -27,8 +34,6 @@ class Simulation:
     def do_steps(self, n_steps):
         if self.is_busy():
             return
-        #otherwise
-        self.semaphore.acquire()  # Acquire the semaphore before starting the steps
         try:
             if n_steps == -1:
                 threading.Thread(target=self.execute_all_steps).start()
@@ -38,24 +43,25 @@ class Simulation:
             self.semaphore.release()  # Release the semaphore if thread creation fails
 
 
-    def simulation_step(self, n_steps):
-        if self.is_busy():
-            return "Simulation is busy"
-        self.n_steps = n_steps
-        self.do_steps(n_steps)
-        return "Simulation step executed"
-
     def execute_steps(self, n_steps):
+        self.semaphore.acquire()
         try:
             for step in range(n_steps):
-                libsumo.simulationStep()
+                if not self.kill_thread:
+                    libsumo.simulationStep()
+                else:
+                    break
         # This ensures that when all cars are done, or if something goes wrong, the track (semaphore) is free for someone else to use
         finally:
-            self.semaphore.release()  # Ensure semaphore is released even if an exception occurs
+            self.semaphore.release()
+            self.kill_thread=False # Ensure semaphore is released even if an exception occurs
 
     def execute_all_steps(self):
+        self.semaphore.acquire()
         try:
-            while libsumo.simulation.getMinExpectedNumber() > 0:  # libsumo.simulation.getMinExpectedNumber() returns the number of cars expected to be on the track.
+            while libsumo.simulation.getMinExpectedNumber() > 0 and not self.kill_thread:  # libsumo.simulation.getMinExpectedNumber() returns the number of cars expected to be on the track.
                 libsumo.simulationStep()                        # This function moves the simulation forward by one step, making the cars move.
-        finally:                                             # finally guarantees the release of the semaphore.
-            self.semaphore.release()                       # Ensure semaphore is released even if an exception occurs
+        finally:
+            self.semaphore.release()
+            self.kill_thread=False
+            # Ensure semaphore is released even if an exception occurs
--- start.py
+++ start.py
@@ -1,16 +1,26 @@
 import os
-
-import ujson
 from flask import Flask, request, make_response
 from os import path, listdir, mkdir
 import gzip
 import xml.etree.ElementTree as ET
 import xmltodict
 import shutil
+import ujson
 from consts import *
 from libraries.simulation import Simulation  # Importing Simulation class from simulation.py
 
 app = Flask(__name__)
+sim_instance = Simulation()  # <- Create simulation instance
+
+
+def error_400(message):
+    return make_response(message, 400)
+
+
+def error_key(key):
+    return error_400("Invalid value for key: " + key)
+
+
 def check_config_files():
     return path.exists(CFG_PATH) and path.isfile(CFG_PATH + SUMOCFG) and path.isfile(
         CFG_PATH + SUMONET) and path.isfile(CFG_PATH + SUMOROUTE) and path.isfile(CFG_PATH + SUMOADD)
@@ -18,141 +28,158 @@ def check_config_files():
 
 @app.route('/network', methods=['PUT'])
 def upload_network():
-    if request.data is None:
-        return "No file part"
-    if len(request.data) > 1e9:
-        return "File too big"
-    file = request.get_data(as_text=True)
-    try:
-        file = ET.fromstring(file)
-    except ET.ParseError:
-        return "Invalid XML file"
-    if file.tag == "net" or file.tag == "network":
-        root = ET.ElementTree(file)
-        with open(path.join(app.config['UPLOAD_FOLDER'], SUMONET), 'wb') as f:
-            root.write(f, xml_declaration=True, encoding="utf-8")
-        return "File uploaded"
-    return "File not allowed"
+    if not sim_instance.is_busy():
+        if request.data is None:
+            return error_400("No file part")
+        if len(request.data) > 1e9:
+            return error_400("File too big")
+        file = request.get_data(as_text=True)
+        try:
+            file = ET.fromstring(file)
+        except ET.ParseError:
+            return error_400("Invalid XML file")
+        if file.tag == "net" or file.tag == "network":
+            root = ET.ElementTree(file)
+            with open(path.join(app.config['UPLOAD_FOLDER'], SUMONET), 'wb') as f:
+                root.write(f, xml_declaration=True, encoding="utf-8")
+            return "File uploaded"
+        return error_400("File not allowed")
+    else:
+        return error_400("Simulation is running, cannot modify network!")
 
 
 @app.route('/routes', methods=['PUT'])
 def upload_routes():
-    if request.data is None:
-        return "No file part"
-    if len(request.data) > 1e9:
-        return "File too big"
-    file = request.get_data(as_text=True)
-    try:
-        file = ET.fromstring(file)
-    except ET.ParseError:
-        return "Invalid XML file"
-    if file.tag == "routes":
-        root = ET.ElementTree(file)
-        with open(path.join(app.config['UPLOAD_FOLDER'], SUMOROUTE), 'wb') as f:
-            root.write(f, xml_declaration=True, encoding="utf-8")
-        return "File uploaded "
-    return "File not allowed"
+    if not sim_instance.is_busy():
+        if request.data is None:
+            return error_400("No file part")
+        if len(request.data) > 1e9:
+            return error_400("File too big")
+        file = request.get_data(as_text=True)
+        try:
+            file = ET.fromstring(file)
+        except ET.ParseError:
+            return error_400("Invalid XML file")
+        if file.tag == "routes":
+            root = ET.ElementTree(file)
+            with open(path.join(app.config['UPLOAD_FOLDER'], SUMOROUTE), 'wb') as f:
+                root.write(f, xml_declaration=True, encoding="utf-8")
+            return "File uploaded "
+        return error_400("File not allowed")
+    else:
+        return error_400("Simulation is running, cannot modify routes!")
 
 
 @app.route('/charging_stations', methods=['PUT'])
 def upload_stations():
-    reset = request.args.get('reset', default=False)
-    if request.data is None:
-        return "No file part"
-    if len(request.data) > 1e9:
-        return "File too big"
-    file = request.get_data(as_text=True)
-    try:
-        file = ET.fromstring(file)
-    except ET.ParseError:
-        return "Invalid XML file"
-    if file.tag == "additional":
-        if reset or not path.exists(CFG_PATH + SUMOADD):
-            if path.exists(CFG_PATH + SUMOADD):
-                os.remove(CFG_PATH + SUMOADD)
-            root = ET.ElementTree(file)
-            with open(path.join(app.config['UPLOAD_FOLDER'], SUMOADD), 'wb') as f:
-                root.write(f, xml_declaration=True, encoding="utf-8")
-        else:
-            existree = ET.parse(CFG_PATH + SUMOADD)
-            oldstations = existree.getroot()
-            for station in file.iter('chargingStation'):
-                oldstations.append(station)
-            existree.write(CFG_PATH + SUMOADD, xml_declaration=True)
-        return "File uploaded"
-    return "File not allowed"
+    if not sim_instance.is_busy():
+        reset = request.args.get('reset', default=False)
+        if request.data is None:
+            return error_400("No file part")
+        if len(request.data) > 1e9:
+            return error_400("File too big")
+        file = request.get_data(as_text=True)
+        try:
+            file = ET.fromstring(file)
+        except ET.ParseError:
+            return error_400("Invalid XML file")
+        if file.tag == "additional":
+            if reset or not path.exists(CFG_PATH + SUMOADD):
+                if path.exists(CFG_PATH + SUMOADD):
+                    os.remove(CFG_PATH + SUMOADD)
+                root = ET.ElementTree(file)
+                with open(path.join(app.config['UPLOAD_FOLDER'], SUMOADD), 'wb') as f:
+                    root.write(f, xml_declaration=True, encoding="utf-8")
+            else:
+                existree = ET.parse(CFG_PATH + SUMOADD)
+                oldstations = existree.getroot()
+                for station in file.iter('chargingStation'):
+                    oldstations.append(station)
+                existree.write(CFG_PATH + SUMOADD, xml_declaration=True)
+            return "File uploaded"
+        return error_400("File not allowed")
+    else:
+        return error_400("Simulation is running, cannot modify stations")
 
 
 @app.route('/battery_options', methods=['POST'])
 def battery_options():
-    if request.get_json() is not None and check_config_files():
-        tree = ET.parse(CFG_PATH + SUMOCFG)
-        root = tree.getroot()
-        if root.tag != "configuration":
-            return "Invalid configuration file"
-        battery_xml = root.find('battery') if root.find('battery') is not None else ET.Element("battery")
-        battery_opts = request.get_json()
-        for key in battery_opts:
-            elem = ET.Element(key)
-            if key in FLOAT_BATT_PARAMS or key in TIME_BATT_PARAMS:
-                if isinstance(battery_opts[key], float) or isinstance(battery_opts[key], int):
-                    elem.set('value', str(battery_opts[key]))
-                    battery_xml.append(elem)
+    if not sim_instance.is_busy():
+        if request.get_json() is not None and check_config_files():
+            tree = ET.parse(CFG_PATH + SUMOCFG)
+            root = tree.getroot()
+            if root.tag != "configuration":
+                return error_400("Invalid configuration file")
+            battery_xml = root.find('battery') if root.find('battery') is not None else ET.Element("battery")
+            battery_opts = request.get_json()
+            for key in battery_opts:
+                elem = ET.Element(key)
+                if key in FLOAT_BATT_PARAMS or key in TIME_BATT_PARAMS:
+                    if isinstance(battery_opts[key], float) or isinstance(battery_opts[key], int):
+                        elem.set('value', str(battery_opts[key]))
+                        battery_xml.append(elem)
+                    else:
+                        return error_key(key)
+                elif key in BOOL_BATT_PARAMS:
+                    if isinstance(battery_opts[key], bool) or (battery_opts[key] == "t" or battery_opts[key] == "f"):
+                        elem.set('value', str(battery_opts[key]).lower())
+                        battery_xml.append(elem)
+                    else:
+                        return error_key(key)
+                elif key in STRING_BATT_PARAMS:
+                    if isinstance(battery_opts[key], str):
+                        elem.set('value', battery_opts[key])
+                        battery_xml.append(elem)
+                    else:
+                        return error_key(key)
+                elif key in STRARR_BATT_PARAMS:
+                    if len(battery_opts[key]) > 0:
+                        elem.set('value', ' '.join(battery_opts[key]))
+                        battery_xml.append(elem)
                 else:
-                    return "Invalid value for " + key
-            elif key in BOOL_BATT_PARAMS:
-                if isinstance(battery_opts[key], bool) or (battery_opts[key] == "t" or battery_opts[key] == "f"):
-                    elem.set('value', str(battery_opts[key]).lower())
-                    battery_xml.append(elem)
-                else:
-                    return "Invalid value for " + key
-            elif key in STRING_BATT_PARAMS:
-                if isinstance(battery_opts[key], str):
-                    elem.set('value', battery_opts[key])
-                    battery_xml.append(elem)
-                else:
-                    return "Invalid value for " + key
-            elif key in STRARR_BATT_PARAMS:
-                if len(battery_opts[key]) > 0:
-                    elem.set('value', ' '.join(battery_opts[key]))
-                    battery_xml.append(elem)
-            else:
-                return "Invalid parameter " + key
-        if root.find('battery') is None:
-            root.append(battery_xml)
-        tree.write(CFG_PATH + SUMOCFG, xml_declaration=True)
-        return "Battery options updated!"
+                    return error_key(key)
+            if root.find('battery') is None:
+                root.append(battery_xml)
+            tree.write(CFG_PATH + SUMOCFG, xml_declaration=True)
+            return "Battery options updated!"
+        else:
+            return error_400("Invalid request, or configuration file not found")
     else:
-        return "Invalid request, or configuration file not found"
+        return error_400("Simulation is running, cannot modify battery options!")
 
 
 @app.route('/output_options', methods=['POST'])
 def output_options():
-    if request.get_json() is not None and check_config_files():
-        tree = ET.parse(CFG_PATH + SUMOCFG)
-        root = tree.getroot()
-        if root.tag != "configuration":
-            return "Invalid configuration file"
-        output_xml = root.find('output') if root.find('output') is not None else ET.Element("output")
-        output_opts = request.get_json()
-        for key in output_opts:
-            elem = ET.Element(key)
-            if key in OUTPUT_OPTS and key not in DEF_OUTPUT_OPTS:
-                elem.set('value', str(output_opts[key]))
-                output_xml.append(elem)
-            else:
-                return "Invalid parameter " + key
-        if root.find('output') is None:
-            root.append(output_xml)
-        tree.write(CFG_PATH + SUMOCFG, xml_declaration=True)
-        return "Output options updated!"
+    if not sim_instance.is_busy():
+        if request.get_json() is not None and check_config_files():
+            tree = ET.parse(CFG_PATH + SUMOCFG)
+            root = tree.getroot()
+            if root.tag != "configuration":
+                return error_400("Invalid configuration file")
+            output_xml = root.find('output') if root.find('output') is not None else ET.Element("output")
+            output_opts = request.get_json()
+            for key in output_opts:
+                elem = ET.Element(key)
+                if key in OUTPUT_OPTS and key not in DEF_OUTPUT_OPTS:
+                    elem.set('value', str(output_opts[key]))
+                    output_xml.append(elem)
+                else:
+                    return error_key(key)
+            if root.find('output') is None:
+                root.append(output_xml)
+            tree.write(CFG_PATH + SUMOCFG, xml_declaration=True)
+            return "Output options updated!"
+        else:
+            return error_400("Invalid request, or configuration file not found")
     else:
-        return "Invalid request, or configuration file not found"
+        return error_400("Simulation is running, cannot modify output options!")
 
 
 @app.route('/init_simulation')
 def init_simulation():
     """resets simulation files"""
+    if sim_instance.is_busy():
+        return error_400("Another simulation is running, cannot reset files!")
     if path.exists(CFG_PATH):
         shutil.rmtree(CFG_PATH)
     os.mkdir(CFG_PATH)
@@ -164,61 +191,58 @@ def init_simulation():
         shutil.copyfile(DEF_PATH + SUMOROUTE, CFG_PATH + SUMOROUTE)
     if not (path.isfile(CFG_PATH + SUMOADD)):
         shutil.copyfile(DEF_PATH + SUMOADD, CFG_PATH + SUMOADD)
-    return "Simulation initialized" if check_config_files() else "Error initializing simulation"
+    return make_response("Simulation initialized", 200) if check_config_files() else make_response(
+        "Error initializing simulation", 500)
 
 
 @app.route('/start_simulation')
 def start_simulation():
-    global sim_instance  # <------ Use global variable sim_instance
     try:
+        if sim_instance.is_busy():
+            return error_400("Another simulation is running")
         if check_config_files():
-            begin = request.args.get('begin', default=0)
-            end = request.args.get('end', default=None)
-            step_duration = request.args.get('step_duration', default=1)  # in seconds
-            n_steps = request.args.get('n_steps', default=-1)
-            sim_instance = Simulation(begin, end, step_duration, n_steps)  # <------ Create Simulation instance
+            begin = request.args.get('begin', default=0, type=int)
+            end = request.args.get('end', default=None, type=int)
+            step_duration = request.args.get('step_duration', default=1, type=int)  # in seconds
+            n_steps = request.args.get('n_steps', default=-1, type=int)
+            sim_instance.configure(begin, end, step_duration, n_steps)  # <------ Configure Simulation instance
             return "Simulation started"
         else:
-            return "Error starting simulation, configuration files not found"
+            return make_response("Error starting simulation, configuration files not found", 500)
     except Exception as e:
         #app.logger.error(f"Error starting simulation: {str(e)}")
-        return f"Error starting simulation: {str(e)}"
+        return make_response(f"Error starting simulation: {str(e)}", 500)
 
 
 @app.route('/next_step')
 def next_step():
-    global sim_instance  # <------ Use global variable sim_instance
-    if sim_instance:
-        n = request.args.get('n')
-        sim_instance.simulation_step(int(n))  # Example call to simulation_step method of Simulation class
+    if not sim_instance.is_busy():
+        n = request.args.get('n', default=sim_instance.n_steps, type=int)
+        sim_instance.do_steps(int(n))  # Example call to simulation_step method of Simulation class
         return "Next step taken"
     else:
-        return "Simulation not yet started"
+        return error_400("Simulation is busy")
 
 
 @app.route('/stop_simulation')
 def stop_simulation():
-    global sim_instance  # <------ Use global variable sim_instance
     if sim_instance:
         sim_instance.stop_simulation()
-        sim_instance = None  # <------ Reset sim_instance
-        return "Simulation stopped"
-    else:
-        return "Simulation not running"
+        return "Stopped Simulation"
 
 
 @app.route('/status')
 def status():
-    global sim_instance  # <------ Use global variable sim_instance
-    if sim_instance:
+    if sim_instance.is_busy():
         return "Simulation running"
     else:
         return "Simulation not running"
 
 
 @app.route('/outputs')
-@compress.compressed()
 def results():
+    if sim_instance.is_busy():
+        return error_400("sim_instance is running, wait until the end of it to get outputs")
     if len(request.args) == 0:
         available = []
         if path.exists(OUTPUT_PATH):
@@ -231,11 +255,11 @@ def results():
         files = [el.rsplit('.', 1)[0] for el in listdir(OUTPUT_PATH) if el.endswith('.xml')]
         for key in request.args:
             if key in files:
-                with open(OUTPUT_PATH+key+'.xml') as f:
-                    xmldict=xmltodict.parse(f.read())
-                    body.append({key:xmldict})
-        content=gzip.compress(ujson.dumps(body).encode('utf8'), COMPRESSION_LEVEL)
-        response= make_response(content)
+                with open(OUTPUT_PATH + key + '.xml') as f:
+                    xmldict = xmltodict.parse(f.read())
+                    body.append({key: xmldict})
+        content = gzip.compress(ujson.dumps(body).encode('utf8'), COMPRESSION_LEVEL)
+        response = make_response(content)
         response.headers['Content-length'] = len(content)
         response.headers['Content-Encoding'] = 'gzip'
         return response
